d844ae796b61fc8f262d111fed100ab7d923cda8
Ebenezer Jay Ingalsbe <[ebenezerjaymusic@gmail.com]>
2020-03-29 10:57:17 -0600

    Add php files

---
+++ indieMain.js
@@ -0,0 +1,9 @@
+const leftMainArticle = $(".left-main");
+const rightMainArticle = $(".right-main");
+const nameInput = $("#form-name-id");
+const dateInput = $("#form-name-id");
+const genreInput = $("#form-name-id");
+const platformInput = $("#form-name-id");
+const submitBtn = $("#form-submit-btn-id");
+
+
