ac0acbebb7d50a827cf6d7fe381634acbc4cad3e
Ebenezer Jay Ingalsbe <[ebenezerjaymusic@gmail.com]>
2020-04-07 16:21:35 -0600

    Add ajax call with table css

--- submitForm.php
+++ submitForm.php
@@ -1,6 +1,6 @@
 <?php
 // connect to database
-$submitData = new mysqli("localhost:3306", "indieliv_eb", "Lando2112!", "indieliv_submissions");
+$submitData = new mysqli("localhost:3306", "indieliv_eb", "Helpontheway2112!", "indieliv_Streams");
 
 // check connection
 if ($submitData->connect_error) {
@@ -9,18 +9,30 @@ if ($submitData->connect_error) {
 
 // get input data
 $name = $_POST['formName'];
+$date = $_POST['formStreamDate'];
+$genre = $_POST['formGenre'];
+$time = $_POST['formStreamTime'];
+$platform = $_POST['formPlatform'];
+$link = $_POST['formStreamLink'];
+
+// message to user after submission
+$message = "Thanks for submitting " . $name . "! " . "Your link " . $link . " can be found in the " . $genre . " category. ";
+
+$submissionData = "INSERT INTO streamerData (artistName,streamDate,streamTime,link,genre,platform) VALUES ('{$submitData->real_escape_string($_POST['formName'])}',
+'{$submitData->real_escape_string($_POST['formStreamDate'])}',
+'{$submitData->real_escape_string($_POST['formStreamTime'])}',
+'{$submitData->real_escape_string($_POST['formStreamLink'])}',
+'{$submitData->real_escape_string($_POST['formGenre'])}',
+'{$submitData->real_escape_string($_POST['formPlatform'])}')";
+
+$insertData = $submitData->query($submissionData); 
 
 
-
-
-
-
-
-
-
-
-
-
+if ($insertData) {
+    echo $message;
+} else {
+    die("Error: {$submitData->errno} : {$submitData->error}");
+}
 
 
 ?>
