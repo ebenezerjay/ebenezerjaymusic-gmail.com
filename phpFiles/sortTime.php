1bd7636f1981725ebaf18880f03075f1f5a69709
Ebenezer Jay Ingalsbe <[ebenezerjaymusic@gmail.com]>
2020-04-16 11:19:59 -0600

    Add column sort and nav styles

---
+++ phpFiles/sortTime.php
@@ -0,0 +1,27 @@
+<?php
+// make connection to database
+	$sortDisplayData = new mysqli("localhost:3306", "indieliv_eb", "Helpontheway2112!", "indieliv_Streams");
+			
+// check connection
+if ($sortDisplayData->connect_error) {
+	die('Connect error: ' . $sortDisplayData->connect_errno . ': ' . $sortDisplayData->connect_error);
+} 
+
+// select table data and sort by name
+$timeSortQuery = mysqli_query($sortDisplayData,"SELECT artistName, streamDate, streamTime, link, genre, platform FROM streamerData ORDER BY streamTime");
+
+// output data of each row
+if(mysqli_num_rows($timeSortQuery) > 0) {
+	$submissions = mysqli_fetch_all($timeSortQuery,MYSQLI_ASSOC);
+	foreach($submissions as $submission) : ?>
+		<tr id="tr-id-time-sort">
+			<td dragable="true" class="td-name"><?php echo $submission['artistName']; ?> </td>
+			<td dragable="true" class="td-date"><?php echo $submission['streamDate']; ?> </td>
+			<td dragable="true" class="td-time"><?php echo $submission['streamTime']; ?> </td>
+			<td dragable="true" class="td-link"><?php echo $submission['link']; ?> </td>
+			<td dragable="true" class="td-genre"><?php echo $submission['genre']; ?> </td>
+			<td dragable="true" class="td-platform"><?php echo $submission['platform']; ?> </td>
+		</tr>
+		<?php endforeach;
+ }
+?>
