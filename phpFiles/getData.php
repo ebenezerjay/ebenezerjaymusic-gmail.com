4ea75997e25b2cdf296be1d3195af01d808bac69
Ebenezer Jay Ingalsbe <[ebenezerjaymusic@gmail.com]>
2020-05-06 10:55:20 -0600

    Add archive pages

--- phpFiles/getData.php
+++ phpFiles/getData.php
@@ -9,20 +9,23 @@ if ($retreiveData->connect_error) {
 
 // select table data
 $sel = mysqli_query($retreiveData,"SELECT * FROM streamerData ORDER BY streamDate limit 20");
+$selLink = mysqli_query($retreiveData,"SELECT link from streamerData");
 
 // loop through table data on database and insert into dom table
  if(mysqli_num_rows($sel) > 0) {
 	$submissions = mysqli_fetch_all($sel,MYSQLI_ASSOC);
 	foreach($submissions as $submission) : ?>
 		<tr id="tr-id">
-			<td dragable="true" class="td-name"><?php echo $submission['artistName']; ?> </td>
-			<td dragable="true" class="td-date"><?php echo $submission['streamDate']; ?> </td>
-			<td dragable="true" class="td-time"><?php echo $submission['streamTime']; ?> </td>
-			<td dragable="true" class="td-link"><?php echo $submission['link']; ?> </td>
-			<td dragable="true" class="td-genre"><?php echo $submission['genre']; ?> </td>
-			<td dragable="true" class="td-platform"><?php echo $submission['platform']; ?> </td>
+			<td class="td-name"><?php echo $submission['artistName']; ?> </a></td>
+			<td class="td-date"><?php echo $submission['streamDate']; ?> </td>
+			<td class="td-time"><?php echo $submission['streamTime']; ?> </td>
+			<td class="td-link"> <a href=""> <?php echo $submission['link']; ?></a> </td>
+			<td class="td-genre"><?php echo $submission['genre']; ?> </td>
+			<td class="td-platform"><?php echo $submission['platform']; ?> </td>
 		</tr>
 		<?php endforeach;
  }
+
+
 
 ?>
