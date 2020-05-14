4f1391ea2464ce4273aae3ac6de1f506c320cda7
Ebenezer Jay Ingalsbe <[ebenezerjaymusic@gmail.com]>
2020-05-14 14:20:25 -0600

    Make links clickable

--- archive.js
+++ archive.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 		});
 	});
 
+
 // sends ajax call to search database by name
 // $( "#form-search-id" ).submit(function(e) {
 //     e.preventDefault();
@@ -15,3 +16,37 @@ $(document).ready(function() {
 //           $("tbody").html(data);
 //       });
 //   });
+
+// displays site info from nav
+$(document).ready(function() {
+	$("#li-about-id").on('click', function() {
+		let articleAbout = $("#about-paragraph-id");
+		$(".about-paragraph").css("padding", "10px");
+		$(articleAbout).toggleClass("about-paragraph");
+	});
+});
+
+// displays site info from footer
+$(document).ready(function() {
+	$("#about-footer-id").on('click', function() {
+		let articleAbout = $("#about-paragraph-id");
+		$(".about-paragraph").css("padding", "10px");
+		$(articleAbout).toggleClass("about-paragraph");
+	});
+});
+
+// displays the comment/feedback form from nav
+$(document).ready(function() {
+	$("#li-feedback-id").on('click', function() {
+		let commentForm = $("#form-contact-id");
+		$(commentForm).toggleClass("form-comment");
+	});
+});
+
+// displays the comment feedback form from footer
+$(document).ready(function() {
+	$("#contact-footer-id").on('click', function() {
+		let commentForm = $("#form-contact-id");
+		$(commentForm).toggleClass("form-comment");
+	});
+});
