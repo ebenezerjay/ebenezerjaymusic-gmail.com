47ae7e1ff290751e042b430762a0f9c28d97cfff
Ebenezer Jay Ingalsbe <[ebenezerjaymusic@gmail.com]>
2020-06-11 11:52:46 -0600

    Add media queries and php archive files

--- phpFiles/sortGenre.php
+++ phpFiles/sortGenre.php
@@ -15,12 +15,12 @@ if(mysqli_num_rows($genreSortQuery) > 0) {
 	$submissions = mysqli_fetch_all($genreSortQuery,MYSQLI_ASSOC);
 	foreach($submissions as $submission) : ?>
 		<tr id="tr-id-genre-sort">
-			<td dragable="true" class="td-name"><?php echo $submission['artistName']; ?> </td>
-			<td dragable="true" class="td-date"><?php echo $submission['streamDate']; ?> </td>
-			<td dragable="true" class="td-time"><?php echo $submission['streamTime']; ?> </td>
-			<td dragable="true" class="td-link"><a href="<?php echo $submission['link'];?>">Web Address For Live Stream</a></td>
-			<td dragable="true" class="td-genre"><?php echo $submission['genre']; ?> </td>
-			<td dragable="true" class="td-platform"><?php echo $submission['platform']; ?> </td>
+			<td class="td-name"><?php echo $submission['artistName']; ?> </td>
+			<td class="td-date"><?php echo $submission['streamDate']; ?> </td>
+			<td class="td-time"><?php echo $submission['streamTime']; ?> </td>
+			<td class="td-link"><a href="<?php echo $submission['link'];?>">Web Address For Live Stream</a></td>
+			<td class="td-genre"><?php echo $submission['genre']; ?> </td>
+			<td class="td-platform"><?php echo $submission['platform']; ?> </td>
 		</tr>
 		<?php endforeach;
  }
